1d52f81a892ce09650bea4f1abd8b85e0384b347
GKephart <gkephart@cnm.edu>
2018-08-29 23:14:51 -0600

    sandbox cleaned up so I can pass it off to Dylan

--- test-encrypt.php
+++ test-encrypt.php
@@ -4,10 +4,10 @@ require_once("aes256.php");
 
 $originalPlaintext = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
 $cipherText = aes256Encrypt($originalPlaintext, "password");
-echo "binary WAT: " . $cipherText;
-$hexCipherText = bin2hex($cipherText);
+echo "binary WAT: " . $cipherText . PHP_EOL;
+$hexCipherText = bin2hex($cipherText) . PHP_EOL;
 echo"hex cipherText: " . $hexCipherText . PHP_EOL;
-echo "bullshit decryption: " . $bullshit =aes256Decrypt($cipherText, "password");
+echo "bullshit decryption: " . $bullshit =aes256Decrypt($cipherText, "password"). PHP_EOL;
 
 
 
