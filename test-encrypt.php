f796f55b91243704dbead3395994b2219a2c1e12
GKephart <gkephart@cnm.edu>
2018-08-29 22:09:48 -0600

    fudging around to see if I can can find a  salt. looks like there is no salt

--- test-encrypt.php
+++ test-encrypt.php
@@ -2,10 +2,24 @@
 
 require_once("aes256.php");
 
-$plaintext = bin2hex(random_bytes(100));
+for($i = 1; $i <= 5; $i++) {
 
-echo "plain text" . PHP_EOL;
-echo $plaintext . PHP_EOL;
-echo "cipher text" . PHP_EOL;
-echo aes256Encrypt($plaintext, "password" );
+	$plaintext = bin2hex(random_bytes(100));
+
+	echo "plain text" . PHP_EOL;
+	echo $plaintext . PHP_EOL;
+	echo PHP_EOL;
+	echo "cipher text" . PHP_EOL;
+	$cipherText = aes256Encrypt($plaintext, "password");
+	echo $cipherText . PHP_EOL;
+	echo PHP_EOL;
+	echo "occurrences of + " . PHP_EOL;
+$explodedArray =	explode($cipherText, "+");
+
+foreach($explodedArray as $element) {
+	echo strlen($element);
+
+}
+	echo PHP_EOL;
+}
 
