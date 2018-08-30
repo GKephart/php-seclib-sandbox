4ef929ef2734110d9a5961474b30ca056140088b
GKephart <gkephart@cnm.edu>
2018-08-29 23:10:20 -0600

    I think I narrowed down the issue. it looks like the decryption is fucking and not decoding the first part of the string correctly.

--- test-encrypt.php
+++ test-encrypt.php
@@ -1,43 +1,13 @@
 <?php
 
 require_once("aes256.php");
-/**
 
-for($i = 1; $i <= 5; $i++) {
-
-	$plaintext = bin2hex(random_bytes(100));
-
-	echo "plain text" . PHP_EOL;
-	echo $plaintext . PHP_EOL;
-	echo PHP_EOL;
-	echo "cipher text" . PHP_EOL;
-	$cipherText = aes256Encrypt($plaintext, "password");
-	echo $cipherText . PHP_EOL;
-	echo PHP_EOL;
-	echo "occurrences of + " . PHP_EOL;
-$explodedArray =	explode($cipherText, "+");
-
-foreach($explodedArray as $element) {
-	echo strlen($element);
-
-}
-	echo PHP_EOL;
-}
- **/
-
-function isBinary($str) {
-	return preg_match('~[^\x20-\x7E\t\r\n]~', $str) > 0;
-}
-
-
-$originalPlaintext = "this is my favorite string";
+$originalPlaintext = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
 $cipherText = aes256Encrypt($originalPlaintext, "password");
+echo "binary WAT: " . $cipherText;
 $hexCipherText = bin2hex($cipherText);
-echo $hexCipherText . PHP_EOL;
-
-$bullshit = aes256Decrypt($cipherText, "password");
-
-echo isBinary($bullshit);
+echo"hex cipherText: " . $hexCipherText . PHP_EOL;
+echo "bullshit decryption: " . $bullshit =aes256Decrypt($cipherText, "password");
 
 
 
