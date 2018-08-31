13fd123275564161cff7a017c797bf6ed9091fd1
GKephart <gkephart@cnm.edu>
2018-08-30 23:48:38 -0600

    I am now able to add a salt with using sha3-256. I now need to clean this up and write a unit test for it. after that it is on to file faults

--- aes256.php
+++ aes256.php
@@ -18,7 +18,7 @@ function aes256Encrypt($plaintext, $password) {
 
 	//set the password (according to the documentation this line is equivalent to $cipher->setPassword('whatever', 'pbkdf2', 'sha1', 'phpseclib/salt', 1000, 256 / 8);
 	$salt = bin2hex(random_bytes(36 ));
-	$cipher->setPassword($password,  "sha3-256", $salt);
+	$cipher->setPassword($password, "pbkdf2", "sha3-256", $salt);
 	$iv = bin2hex(random_bytes(32));
 	$cipher->setIV($iv);
 
@@ -55,7 +55,7 @@ function aes256Decrypt( object $ciphertext, $password) {
 	$cipher = new AES();
 
 	//set the password
-	$cipher->setPassword($password);
+	$cipher->setPassword($password, "pbkdf2", "sha3-256", $ciphertext->salt);
 	$iv = Random::string($cipher->getBlockLength() >> 3);
 	var_dump($iv);
 	$cipher->setIV($ciphertext->iv);
@@ -63,7 +63,7 @@ function aes256Decrypt( object $ciphertext, $password) {
 	//decrypt the cipher text
 	$plaintext = $cipher->decrypt($ciphertext->raw);
 
-	if ($plaintext === false) {
+	if ($plaintext === null) {
 		throw new InvalidArgumentException("cipher text could not be encrypted");
 	}
 	return($plaintext);
