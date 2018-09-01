89fadfb7d1fcaf1a9cf2e9bd8cc94e59fe424e43
GKephart <gkephart@cnm.edu>
2018-08-31 23:23:40 -0600

    test is written I just need to run it

--- test-encrypt.php
+++ test-encrypt.php
@@ -2,11 +2,21 @@
 
 require_once("aes256.php");
 
-$originalPlaintext = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
-$cipherText = aes256Encrypt($originalPlaintext, "password");
-//echo "binary WAT: " . $cipherText . PHP_EOL;
-//echo"hex cipherText: " . bin2hex($cipherText) . PHP_EOL;
-echo "bullshit decryption: " . aes256Decrypt($cipherText, "password"). PHP_EOL;
+//setup for the test data and the salt
+$testData = (object)["foo" => "bar", "baz" => "qux", "quux" => "quuz"];
+$jsonfiedTestData = json_decode($testData);
+$salt = bin2hex(random_bytes(32));
+
+
+//run the methods that are being tested
+for($i = 0; $i <= 100; $i++) {
+	try {
+		$cipherText = aes256Encrypt($jsonfiedTestData, "password", $salt);
+		aes256Decrypt($cipherText, "password", $salt);
+	} catch(\InvalidArgumentException $exception) {
+		echo "you done messed up Aaron";
+	}
+}
 
 
 
