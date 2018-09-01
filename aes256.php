1e9d2e34122f462f164154b1b308a40bd203e88c
GKephart <gkephart@cnm.edu>
2018-08-31 22:25:01 -0600

    I am happy with the encrypt method. All that is left is clean up for the decrypt.

--- aes256.php
+++ aes256.php
@@ -7,31 +7,26 @@ use phpseclib\Crypt\Random;
  *
  * @param string $plaintext plaintext to encrypt
  * @param string $password plaintext symmetric key
+ * @param string $salt salt used for encryption and decryption
  * @return string base 64 encoded ciphertext
  * @throws
  * @see http://php.net/manual/en/function.openssl-encrypt.php
  **/
-function aes256Encrypt($plaintext, $password) {
+function aes256Encrypt( string $plaintext, string $password , string $salt) : string {
 
 	//initialize the AES class for php-sec-lib2
 	$cipher = new AES();
 
 	//set the password (according to the documentation this line is equivalent to $cipher->setPassword('whatever', 'pbkdf2', 'sha1', 'phpseclib/salt', 1000, 256 / 8);
-	$salt = bin2hex(random_bytes(36 ));
+
+
+
 	$cipher->setPassword($password, "pbkdf2", "sha3-256", $salt);
 	$iv = bin2hex(random_bytes(32));
 	$cipher->setIV($iv);
+	$cipherText = $cipher->encrypt($plaintext);
 
-
-;
-
-	$rawCipherText = $cipher->encrypt($plaintext);
-	$cipherText = new stdClass();
-	$cipherText->raw=$rawCipherText;
-	$cipherText->iv = $iv;
-	$cipherText->salt = $salt;
-
-
+	$cipherText = $cipherText . "." . $iv;
 
 	if ($cipherText === false) {
 		throw new InvalidArgumentException("plaintext could not be encrypted");
@@ -45,26 +40,27 @@ function aes256Encrypt($plaintext, $password) {
  *
  * @param string $ciphertext base 64 encoded ciphertext
  * @param string $password plaintext symmetric key
+ * @param string $salt salt used for encryption and decryption
  * @return string decrypted plaintext
- * @throws InvalidArgumentException if the plaintext can't be unpadded
+ * @throws InvalidArgumentException if the pla
  * @see http://php.net/manual/en/function.openssl-decrypt.php
  **/
-function aes256Decrypt( object $ciphertext, $password) {
+function aes256Decrypt( string $ciphertext, string $password,string $salt) : string {
 
 	//initialize the AES class
 	$cipher = new AES();
 
 	//set the password
-	$cipher->setPassword($password, "pbkdf2", "sha3-256", $ciphertext->salt);
-	$iv = Random::string($cipher->getBlockLength() >> 3);
-	var_dump($iv);
-	$cipher->setIV($ciphertext->iv);
+	$cipher->setPassword($password, "pbkdf2","sha1", $salt);
+
+	$cipher->setIV();
 
 	//decrypt the cipher text
-	$plaintext = $cipher->decrypt($ciphertext->raw);
+	$plaintext = $cipher->decrypt($ciphertext);
 
-	if ($plaintext === null) {
+	if ($plaintext === false) {
 		throw new InvalidArgumentException("cipher text could not be encrypted");
 	}
+
 	return($plaintext);
 }
